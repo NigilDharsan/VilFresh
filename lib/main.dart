088a908d10b6f9c808eba0ee9208b4560c64ac1e
Nigil Dharsan <harsan.nn@gmail.com>
2024-03-13 01:21:39 +0530

    Api Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:vilfresh/Src/Sign_In_Screen_Ui/SignIn_Screen.dart';
-import 'package:vilfresh/Src/Home_DashBoard_Ui/Home_DashBoard_Screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'Home Screen/Cart_Screen.dart';
-import 'Home Screen/Daily_Subscription_Screen.dart';
-import 'Src/Categories_Ui/Categories_Screen.dart';
-import 'Src/Checkout_Ui/Checkout_Screen.dart';
-import 'Src/Contact_Us_Ui/Contact_Us_Screen.dart';
-import 'Src/Farmer_Detail_Ui/Farmer_Detail_Screen.dart';
 import 'Src/Login_Ui/Login_Screen.dart';
-import 'Src/My_Order_Ui/My_Order_Screen.dart';
-import 'Src/OTP_Verification_Ui/Otp_Verfication_Screen.dart';
-import 'Src/Product_Description_Ui/Product_Description_Screen.dart';
-import 'Src/Refer_And_Earn_Ui/Refer_And_Earn_Screen.dart';
-import 'Src/Sign_Up_Ui/Appartment_Sign_Up_Screen.dart';
-import 'Src/Sign_Up_Ui/Independent_House_Screen.dart';
-import 'Src/Sign_Up_Ui/Sign_Up_Screen1.dart';
-import 'Src/Sign_Up_Ui/Survey_Screen.dart';
-import 'Src/Subscription_Calander_Ui/Subscription_Calander_Screen.dart';
-import 'Src/Subscription_Checkout_Ui/Subscription_CheckOut_Screen.dart';
-import 'Src/Subscription_Detail_Ui/Subscription_Detail_Screen.dart';
-import 'Src/Wallet_History_Ui/Wallet_History_Screen.dart';
-import 'Src/Wallet_Ui/Wallet_Screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -40,8 +20,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Login_Screen() //const MyHomePage(title: 'Flutter Demo Home Page'),
+        home:
+            Login_Screen() //const MyHomePage(title: 'Flutter Demo Home Page'),
         );
   }
 }
-
