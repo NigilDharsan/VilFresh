b6d4f67de40ab9588b4ead0235919a191f62ba23
Nigil Dharsan <harsan.nn@gmail.com>
2024-08-13 19:26:25 +0530

    Daily Subscription Api Updated

--- lib/Home Screen/Daily_Subscription_Screen.dart
+++ lib/Home Screen/Daily_Subscription_Screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vilfresh/Common_Widgets/Custom_App_Bar.dart';
 import 'package:vilfresh/Src/Subscription_Detail_Ui/Subscription_Detail_Screen.dart';
@@ -8,110 +6,160 @@ import 'package:vilfresh/utilits/ApiService.dart';
 import 'package:vilfresh/utilits/Common_Colors.dart';
 import 'package:vilfresh/utilits/Text_Style.dart';
 
-
 class Subscription_Details extends ConsumerStatefulWidget {
   final String? ltrValue;
   final List<String> ltrOptions;
   final bool isMore;
-  const Subscription_Details({Key? key, required this.ltrValue, required this.ltrOptions,required this.isMore}) : super(key: key);
+  const Subscription_Details(
+      {Key? key,
+      required this.ltrValue,
+      required this.ltrOptions,
+      required this.isMore})
+      : super(key: key);
 
   @override
-  ConsumerState<Subscription_Details> createState() => _Subscription_DetailsState();
+  ConsumerState<Subscription_Details> createState() =>
+      _Subscription_DetailsState();
 }
 
 class _Subscription_DetailsState extends ConsumerState<Subscription_Details> {
   String? ltrValue = " ";
   List<dynamic> ltrOptions = [];
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final _categoriesData = ref.watch(CategoriesProvider("1"));
-    final _SubscriptionQntydata = ref.watch(SubscriptionQntyProvider("1"));
+    // final _SubscriptionQntydata = ref.watch(SubscriptionQntyProvider("1"));
 
     return Scaffold(
         backgroundColor: backGround1,
-        appBar: Custom_AppBar(title: "Daily Subscription", actions: [], isNav: widget.isMore == true?true:false, isGreen: false,),
-        body: ListView.builder(
-            itemCount:  4,
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            // physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (BuildContext context, int index){
-              return Container(
-                margin: EdgeInsets.only(bottom: 20),
-                width: MediaQuery.sizeOf(context).width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      width: MediaQuery.sizeOf(context).width,
-                      color: white2,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 100,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              image: DecorationImage(image: AssetImage("lib/assets/onion1.png"))
+        appBar: Custom_AppBar(
+          title: "Daily Subscription",
+          actions: [],
+          isNav: widget.isMore == true ? true : false,
+          isGreen: false,
+        ),
+        body: _categoriesData.when(data: (data) {
+          return ListView.builder(
+              itemCount: 4,
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              // physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.only(top: 10),
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.sizeOf(context).width,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 80,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          data?.data?[index].itemImage ?? ""))),
                             ),
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10, top: 5),
-                                child: Text(
-                                  "Cow Milk",
-                                  style: SubT,
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 10, top: 10, bottom: 5, right: 10),
+                                  child: Text(
+                                    data?.data?[index].item ?? "",
+                                    style: SubT,
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                                 ),
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10,),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text('1 Ltr' + ' - ' + '₹ 33', style:SubT2 ,),
-
-
-                                    InkWell(
-                                      onTap: (){
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    Subscription_Detail_Screen(productname: '', image: '', actualprice: '', catogoryname: '', deliverydate: '', varient: '',)));
-                                      },
-                                      child: Container(
-                                        margin: EdgeInsets.only(left: 100),
-                                        color: green5,
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(5),
-                                          child: Center(child: Text("Subscribe",style: SubT4,)),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    left: 10,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        '1 Ltr' + ' - ' + '₹ 33',
+                                        style: SubT2,
+                                      ),
+                                      InkWell(
+                                        onTap: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      Subscription_Detail_Screen(
+                                                        productname: data
+                                                                ?.data?[index]
+                                                                .item ??
+                                                            "",
+                                                        image: data
+                                                                ?.data?[index]
+                                                                .itemImage ??
+                                                            "",
+                                                        actualprice: data
+                                                                ?.data?[index]
+                                                                .actualPrice ??
+                                                            "",
+                                                        catogoryname: data
+                                                                ?.data?[index]
+                                                                .CategoryName ??
+                                                            "",
+                                                        deliverydate: data
+                                                                ?.data?[index]
+                                                                .itemID ??
+                                                            "",
+                                                        varient: data
+                                                                ?.data?[index]
+                                                                .variant ??
+                                                            "",
+                                                      )));
+                                        },
+                                        child: Container(
+                                          margin: EdgeInsets.only(left: 20),
+                                          color: green3,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(5),
+                                            child: Center(
+                                                child: Text(
+                                              "Subscribe",
+                                              style: SubT4,
+                                            )),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-
-
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            }
-        )
-    );
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Divider()
+                    ],
+                  ),
+                );
+              });
+        }, error: (Object error, StackTrace stackTrace) {
+          return Text("ERROR");
+        }, loading: () {
+          return Center(child: CircularProgressIndicator());
+        }));
   }
 }
-
